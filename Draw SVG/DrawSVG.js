a567ed3ed77aef1518344d95d6cb9212b98039ea
Allan Lago <allanlago1@gmail.com>
2020-06-26 23:11:12 -0400

    Only allowed to start transparent if the animation is a composition

--- Draw SVG/DrawSVG.js
+++ Draw SVG/DrawSVG.js
@@ -4,83 +4,87 @@ import React, {
   useImperativeHandle,
   useEffect,
 } from "react";
-import "./Effects.css";
+import "./DrawSVG.css";
 
 function DrawSVG(props, ref) {
   const child = React.Children.only(props.children);
+  //animation state
   const [anim, setAnim] = useState({
     type:
       props.animation && ["comp", "decomp"].indexOf(props.animation) >= 0
         ? props.animation
-        : "comp", //function
+        : "comp", //animation type
     duration: props.duration ? props.duration : 2000, //animation duration
     delay: props.delay ? props.delay : 0, //animation delay
-    outlineOnly: props.outlineOnly
-      ? props.outlineOnly.toLowerCase() === "true"
-      : false, //if only the outline should be drawn
-    easingFunction: props.easingFunction
-      ? props.easingFunction
-      : "cubic-bezier(1, 0.16, 1, 0.3)",
+    easingFunction: props.easingFunction ? props.easingFunction : "ease", //easing function of outline animation
+    disableFilling: props.disableFilling ? props.disableFilling : false, //if only the outline should be drawn
+    startTransparent: props.startTransparent ? props.startTransparent : false, //if the svg should be transparent before animation starts
   });
-  const undraw = props.undraw;
 
+  //controls 'universal' undraw commands with the state hook 'undraw'
   useEffect(() => {
-    if (undraw) {
+    if (props.undraw) {
       setAnim({
         type: "decomp",
-        ...undraw,
+        ...props.undraw,
       });
     }
-  }, [undraw]);
+  }, [props.undraw]);
 
   //Permits use of these functions from parent component
   useImperativeHandle(ref, () => ({
     playStartAnimation(
       duration = 2000,
-      delay = 2000,
-      outlineOnly = false,
-      easingFunction = "cubic-bezier(1, 0.16, 1, 0.3)"
+      delay = 0,
+      easingFunction = "ease",
+      disableFilling = false,
+      startTransparent = false
     ) {
       setAnim({
         type: "comp",
         duration: duration,
         delay: delay,
-        outlineOnly: outlineOnly,
         easingFunction: easingFunction,
+        disableFilling: disableFilling,
+        startTransparent: startTransparent,
       });
       return new Promise((resolve) => setTimeout(resolve, duration + delay));
     },
     playEndAnimation(
       duration = 2000,
       delay = 0,
-      outlineOnly = false,
-      easingFunction = "cubic-bezier(0.16, 1, 0.3, 1)"
+      easingFunction = "ease",
+      disableFilling = false,
+      startTransparent = false
     ) {
       setAnim({
         type: "decomp",
         duration: duration,
         delay: delay,
-        outlineOnly: outlineOnly,
         easingFunction: easingFunction,
+        disableFilling: disableFilling,
+        startTransparent: startTransparent,
       });
       return new Promise((resolve) => setTimeout(resolve, duration + delay));
     },
   }));
 
-  let animations = `${anim.type}-outline-svg ${anim.duration}ms ${
-    anim.easingFunction
-  } ${anim.delay}ms forwards${
-    !anim.outlineOnly // if not outline-only
-      ? `, ${anim.type}-fill-svg ${anim.duration}ms ease
-        ${anim.delay}ms forwards`
-      : ""
-  }`;
+  const outlineAnim = `${anim.type}-outline-svg ${anim.duration}ms ${anim.easingFunction} ${anim.delay}ms forwards`;
+  const fillingAnim = `${anim.type}-fill-svg ${anim.duration}ms ease ${anim.delay}ms forwards`;
+  const fillDelayAnim = `delay-fill ${anim.delay}ms`;
+
+  const fullAnimation =
+    outlineAnim +
+    (!anim.disableFilling ? `, ${fillingAnim}` : "") +
+    (anim.type === "comp" && !anim.disableFilling && anim.startTransparent
+      ? `, ${fillDelayAnim}`
+      : "");
 
   return (
     <span
       className={anim.type}
       style={{
-        animation: animations,
+        animation: fullAnimation,
         ...props.style,
       }}
     >
