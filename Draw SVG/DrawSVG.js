1521fba84627c743579f44cf113b2a0a3d1ee9fb
Allan Lago <allanlago1@gmail.com>
2020-06-29 14:51:40 -0400

    DrawSVG is no longer dependant on pathLength

--- Draw SVG/DrawSVG.js
+++ Draw SVG/DrawSVG.js
@@ -3,23 +3,47 @@ import React, {
   forwardRef,
   useImperativeHandle,
   useEffect,
+  useMemo,
 } from "react";
+import { renderToString } from "react-dom/server";
 import "./DrawSVG.css";
 
+//fixes an issue were old safari browsers could not use the argument pathLength
+//in the process eliminates the need to write out the pathLength argument
+const fixChildPathLengths = (child) => {
+  let childParsedString = new DOMParser().parseFromString(
+    renderToString(child),
+    "text/html"
+  );
+  for (let pathElem of childParsedString.getElementsByTagName("path")) {
+    let path_length = pathElem.getTotalLength();
+    pathElem.setAttribute(
+      "style",
+      `--pathLength:${path_length};${pathElem.style.cssText}`
+    );
+  }
+  let htmlValue = childParsedString.getElementsByTagName("svg")[0].outerHTML;
+  return htmlValue;
+};
+
 function DrawSVG(props, ref) {
-  const child = React.Children.only(props.children);
   //animation state
   const [anim, setAnim] = useState({
     type:
       props.animation && ["comp", "decomp"].indexOf(props.animation) >= 0
         ? props.animation
-        : "comp", //animation type
+        : "comp", //function
     duration: props.duration ? props.duration : 2000, //animation duration
     delay: props.delay ? props.delay : 0, //animation delay
     easingFunction: props.easingFunction ? props.easingFunction : "ease", //easing function of outline animation
     disableFilling: props.disableFilling ? props.disableFilling : false, //if only the outline should be drawn
     startTransparent: props.startTransparent ? props.startTransparent : false, //if the svg should be transparent before animation starts
   });
+
+  const processedChild = useMemo(
+    () => fixChildPathLengths(React.Children.only(props.children)),
+    [props.children]
+  );
 
   //controls 'universal' undraw commands with the state hook 'undraw'
   useEffect(() => {
@@ -87,9 +111,8 @@ function DrawSVG(props, ref) {
         animation: fullAnimation,
         ...props.style,
       }}
-    >
-      {child}
-    </span>
+      dangerouslySetInnerHTML={{ __html: processedChild }}
+    />
   );
 }
 
