2ff8b05b385e041e75c4f11a97a1b6314163a647
Allan Lago <allanlago1@gmail.com>
2020-08-26 15:39:03 -0400

    Added modules, object destructuring for animations parameters, and more

--- Draw SVG/DrawSVG.js
+++ Draw SVG/DrawSVG.js
@@ -3,22 +3,23 @@ import React, {
   forwardRef,
   useImperativeHandle,
   useEffect,
+  useCallback,
+  useMemo,
 } from "react";
-import "./DrawSVG.css";
+import module_styles from "./DrawSVG.module.css";
 
 function DrawSVG(props, ref) {
-  const child = React.Children.only(props.children);
   //animation state
   const [anim, setAnim] = useState({
     type:
       props.animation && ["comp", "decomp"].indexOf(props.animation) >= 0
         ? props.animation
         : "comp", //function
-    duration: props.duration ? props.duration : 2000, //animation duration
-    delay: props.delay ? props.delay : 0, //animation delay
-    easingFunction: props.easingFunction ? props.easingFunction : "ease", //easing function of outline animation
-    disableFilling: props.disableFilling ? props.disableFilling : false, //if only the outline should be drawn
-    startTransparent: props.startTransparent ? props.startTransparent : false, //if the svg should be transparent before animation starts
+    duration: props.duration ?? 2000, //animation duration
+    delay: props.delay ?? 0, //animation delay
+    easingFunction: props.easingFunction ?? "ease", //easing function of outline animation
+    disableFilling: props.disableFilling ?? false, //if only the outline should be drawn
+    startTransparent: props.startTransparent ?? false, //if the svg should be transparent before animation starts
   });
 
   //controls 'universal' undraw commands with the state hook 'undraw'
@@ -33,13 +34,13 @@ function DrawSVG(props, ref) {
 
   //Permits use of these functions from parent component
   useImperativeHandle(ref, () => ({
-    playStartAnimation(
+    playStartAnimation({
       duration = 2000,
       delay = 0,
       easingFunction = "ease",
       disableFilling = false,
-      startTransparent = false
-    ) {
+      startTransparent = false,
+    }) {
       setAnim({
         type: "comp",
         duration: duration,
@@ -50,13 +51,13 @@ function DrawSVG(props, ref) {
       });
       return new Promise((resolve) => setTimeout(resolve, duration + delay));
     },
-    playEndAnimation(
+    playEndAnimation({
       duration = 2000,
       delay = 0,
       easingFunction = "ease",
       disableFilling = false,
-      startTransparent = false
-    ) {
+      startTransparent = false,
+    }) {
       setAnim({
         type: "decomp",
         duration: duration,
@@ -69,28 +70,50 @@ function DrawSVG(props, ref) {
     },
   }));
 
-  const outlineAnim = `${anim.type}-outline-svg ${anim.duration}ms ${anim.easingFunction} ${anim.delay}ms forwards`;
-  const fillingAnim = `${anim.type}-fill-svg ${anim.duration}ms ease ${anim.delay}ms forwards`;
-  const fillDelayAnim = `delay-fill ${anim.delay}ms`;
+  const computeAnimation = useCallback(() => {
+    // animation names with module hashes
+    const outlineAnimName = module_styles[`${anim.type}-outline-svg`];
+    const fillingAnimName = module_styles[`${anim.type}-fill-svg`];
+    const fillDelayAnimName = module_styles["delay-fill"];
 
-  const fullAnimation =
-    outlineAnim +
-    (!anim.disableFilling ? `, ${fillingAnim}` : "") +
-    (anim.type === "comp" && !anim.disableFilling && anim.startTransparent
-      ? `, ${fillDelayAnim}`
-      : "");
+    // css animation description
+    const outlineAnim = `${outlineAnimName} ${anim.duration}ms ${anim.easingFunction} ${anim.delay}ms forwards`;
+    const fillingAnim = `${fillingAnimName} ${anim.duration}ms ease ${anim.delay}ms forwards`;
+    const fillDelayAnim = `${fillDelayAnimName} ${anim.delay}ms`;
+
+    const fullAnimation =
+      outlineAnim +
+      (!anim.disableFilling ? `, ${fillingAnim}` : "") +
+      (anim.type === "comp" && !anim.disableFilling && anim.startTransparent
+        ? `, ${fillDelayAnim}`
+        : "");
+
+    return fullAnimation;
+  }, [anim]);
+
+  const animationDescription = useMemo(() => computeAnimation(), [
+    computeAnimation,
+  ]);
 
   return (
     <span
-      className={anim.type}
+      className={`${module_styles[anim.type]} ${props.className ?? ""}`}
       style={{
-        animation: fullAnimation,
+        animation: animationDescription,
         ...props.style,
       }}
+      ref={ref}
     >
-      {child}
+      {React.Children.only(props.children)}
     </span>
   );
 }
 
-export default forwardRef(DrawSVG);
+export default React.memo(forwardRef(DrawSVG), (prevProps, nextProps) => {
+  for (const param in prevProps) {
+    if (prevProps[param] !== nextProps[param]) {
+      return false;
+    }
+  }
+  return true;
+});
